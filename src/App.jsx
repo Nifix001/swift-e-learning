e0c65eb49ce7560e84be728b9ff88a2abb18c76d
Nifix001 <101341614+Nifix001@users.noreply.github.com>
2023-06-02 11:44:18 +0100

    New commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,17 @@ import {
   RouterProvider,
 } from "react-router-dom";
 
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    loader: rootLoader,
+    children: [
+    ],
+  },
+]);
+
 function App() {
   return (
     <div className="App"> 
