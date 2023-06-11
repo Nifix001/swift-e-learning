ea5ed73b472b6fcb931ca3d6277728356f4f6807
Nifix001 <101341614+Nifix001@users.noreply.github.com>
2023-06-11 21:57:50 +0100

    New commit

--- src/layouts/LayoutPage.jsx
+++ src/layouts/LayoutPage.jsx
@@ -7,6 +7,9 @@ const LayoutPage = () => {
     <img src={logo} alt="logo" />
       <nav>
         <NavLink>Home</NavLink>
+        <NavLink>Courses</NavLink>
+        <NavLink>Pricing</NavLink>
+        <NavLink>Testimonials</NavLink>
       </nav>
 
       <main>
