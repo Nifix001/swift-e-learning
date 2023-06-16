ffdb2ef29b15469cd1e13e81bf19102e6ecfed12
Nifix001 <101341614+Nifix001@users.noreply.github.com>
2023-06-16 09:59:35 +0100

    New commit

---
+++ src/components/Instructors.jsx
@@ -0,0 +1,16 @@
+import star from "../assets/Group 5.jpg"
+
+const Instructors = () => {
+  return (
+    <div>
+      <span><img src={star} alt="" /></span>
+      <h2>Gain knowledge from <span>expert instructors</span><br /> at your own pace, anytime and anywhere</h2>
+      <p>Unlock your potential with our engaging e-learning platform. Access high quality courses <br />anytime, anywhere and acheive your personal and professional goals with ease.</p>
+      <span><img src={star} alt="" /></span>
+      <button>Start Learning</button>
+
+    </div>
+  )
+}
+
+export default Instructors
