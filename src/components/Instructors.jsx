2d0c214777d526a26e81be9fc0c115133f0ae4c8
Nifix001 <101341614+Nifix001@users.noreply.github.com>
2023-06-16 10:13:34 +0100

    New commit

--- src/components/Instructors.jsx
+++ src/components/Instructors.jsx
@@ -1,5 +1,11 @@
-import { Outlet } from "react-router-dom"
-import star from "../assets/Group 5.jpg"
+import { Outlet } from "react-router-dom";
+import star from "../assets/Group 5.jpg";
+import img from "../assets/Frame 1000001063.png";
+import img2 from "../assets/Frame 1000001064.png";
+import img3 from "../assets/Frame 1000001065.png";
+import img4 from "../assets/Frame 1000001066.png";
+import img5 from "../assets/Frame 1000001067.png";
+
 
 const Instructors = () => {
   return (
@@ -9,6 +15,14 @@ const Instructors = () => {
       <p>Unlock your potential with our engaging e-learning platform. Access high quality courses <br />anytime, anywhere and acheive your personal and professional goals with ease.</p>
       <span><img src={star} alt="" /></span>
       <button>Start Learning</button>
+
+      <div>
+        <div><img src={img} alt="" /></div>
+        <div><img src={img2} alt="" /></div>
+        <div><img src={img3} alt="" /></div>
+        <div><img src={img4} alt="" /></div>
+        <div><img src={img5} alt="" /></div>
+      </div>
 
       <Outlet />
     </div>
