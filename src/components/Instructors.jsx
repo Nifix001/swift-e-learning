9b5919a4d6e2a7538df24622d97ae04d52e64fa9
Nifix001 <101341614+Nifix001@users.noreply.github.com>
2023-06-17 15:49:20 +0100

    New commit

---
+++ src/components/Instructors.jsx
@@ -0,0 +1,34 @@
+import img from "../assets/IMAGE.png"
+
+const Instructors = () => {
+  return (
+    <div>
+      <h2><span className="span">Meet our Instructors</span></h2>
+      <p>Benefiting from the expertise of the most accomplished professionals in the field</p>
+      <div>
+        <div>
+            <img src={img} alt="" />
+            <h5></h5>
+            <p></p>
+        </div>
+        <div>
+            <img src={} alt="" />
+            <h5></h5>
+            <p></p>
+        </div>
+        <div>
+            <img src={} alt="" />
+            <h5></h5>
+            <p></p>
+        </div>
+        <div>
+            <img src={} alt="" />
+            <h5></h5>
+            <p></p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Instructors
